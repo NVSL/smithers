d93e888c052f5a7db3f36cd9c9ac90cff81ca70d
Steven Swanson <swanson@cs.ucsd.edu>
2017-12-15 11:17:24 -0800

    fix SpooledTemporary file gae problem.  Fix scrolling

--- appengine_config.py
+++ appengine_config.py
@@ -5,5 +5,9 @@ vendor.add(os.path.join(os.path.dirname(__file__),'server'))
 vendor.add(os.path.join(os.path.dirname(__file__),'python_libs'))
 
 
+# GAE doesn't support SpooledTemporaryFile and the normal TemporaryFile doesn't take max_size.  Drop max_size.
 import tempfile
-tempfile.SpooledTemporaryFile = tempfile.TemporaryFile
+def MonkeyTempFile(max_size=None, *args, **kwargs):
+    return tempfile.TemporaryFile(*args, **kwargs)
+
+tempfile.SpooledTemporaryFile = MonkeyTempFile
